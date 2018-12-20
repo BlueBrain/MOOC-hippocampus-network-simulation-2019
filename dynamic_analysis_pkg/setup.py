c5b1097d9595d3a0465fb65da1323a2b1c8509ad
Stefano Antonel <stefano.antonel@epfl.ch>
2018-12-20 14:44:46 +0100

    Add package

---
+++ dynamic_analysis_pkg/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='dynamic_analysis_pkg',
+      version='0.1.2',
+      description='Machinary to run analysis based on simulation url',
+      author='Stefano Antonel',
+      author_email='stefano.antonel@epfl.ch',
+      license='MIT',
+      packages=['dynamic_analysis_pkg'],
+      install_requires=[
+        # 'pyunicore',
+        # 'PyJWT',
+      ],
+      zip_safe=True)
