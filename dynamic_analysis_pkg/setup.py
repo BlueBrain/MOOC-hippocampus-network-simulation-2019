a0c49219ac39653dd821b23d4fd48944638bdbd2
Stefano Antonel <stefano.antonel@epfl.ch>
2020-03-19 13:21:46 +0100

    Update setup.py

--- dynamic_analysis_pkg/setup.py
+++ dynamic_analysis_pkg/setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(name='dynamic_analysis_pkg',
       version='0.1.7',
-      description='Machinary to run analysis based on simulation url',
+      description='Machinary to fetch simulation results to a Jupyter Notebook',
       author='Stefano Antonel',
       author_email='stefano.antonel@epfl.ch',
       license='MIT',
       packages=['dynamic_analysis_pkg'],
       install_requires=[
         'pyunicore',
+        'tabulate',
       ],
       zip_safe=True)
