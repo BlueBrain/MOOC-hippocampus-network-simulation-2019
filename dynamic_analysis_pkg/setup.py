3e6a1850991cef8ba2c90efc4aa0f99336beebdb
Stefano Antonel <stefano.antonel@epfl.ch>
2020-03-23 16:53:35 +0100

    Bump package version

--- dynamic_analysis_pkg/setup.py
+++ dynamic_analysis_pkg/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='dynamic-analysis-pkg',
-      version='0.1.7',
+      version='0.2.0',
       description='Machinary to fetch simulation results to a Jupyter Notebook',
       author='BlueBrain NSE',
       author_email='bbp-ou-nse@groupes.epfl.ch',
